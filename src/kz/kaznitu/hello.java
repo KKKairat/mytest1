37dbde435d16f0848a235b9b6e0e3a99bb7408c8
Kkkairat <rm1697hala@gmail.com>
2017-10-04 12:03:00 +0600

    ekinwi commit

--- src/kz/kaznitu/hello.java
+++ src/kz/kaznitu/hello.java
@@ -3,5 +3,7 @@ package kz.kaznitu;
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("bul zerthanalyktyn maksaty ");
+        System.out.println("git pen jumys jasap uirenu");
     }
 }
